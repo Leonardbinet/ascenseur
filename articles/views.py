dcd6e3d4f520eaaf90cb221e17ded38639d63713
Leonardbinet <leonardbinet@gmail.com>
2016-09-19 09:26:53 +0200

    Harmonisation vue article

--- articles/views.py
+++ articles/views.py
@@ -23,11 +23,7 @@ def article_lecture(request, article_name_slug):
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
         article = Article.objects.get(slug=article_name_slug)
-        context_dict['nom_article'] = article.titre_court
-        context_dict['description_article'] = article.description
-        context_dict['body_article'] = article.body
-        context_dict['meta_desc'] = article.meta_description
-
+        context_dict['article'] = article
 
     except Article.DoesNotExist:
         # We get here if we didn't find the specified article.
@@ -35,6 +31,6 @@ def article_lecture(request, article_name_slug):
         pass
 
     # Go render the response and return it to the client.
-    return render(request, 'articles/article_lecture.html', context_dict)
+    return render(request, 'articles/article.html', context_dict)
 
 
